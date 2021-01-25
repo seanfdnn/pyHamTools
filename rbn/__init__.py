038e70b2dc50f9626fb0906c6388c3f0c4f0ee40
Evan Pratten <ewpratten@gmail.com>
2021-01-25 10:12:56 -0500

    Main app

--- rbn/__init__.py
+++ rbn/__init__.py
@@ -1,1 +1,2 @@
 from .listener import listen_forever
+from .bands import band_mapping
--- rbn/__main__.py
+++ rbn/__main__.py
@@ -1,4 +1,4 @@
-from . import listen_forever
+from . import listen_forever, band_mapping
 import argparse
 import sys
 
@@ -16,7 +16,7 @@ def main() -> int:
         prog="rbn", description="CLI frontend to the Reverse Beacon Network")
     ap.add_argument("-c", "--callsign", help="Your callsign",
                     type=str, required=True)
-    ap.add_argument("-b", "--bands", action="append", help="Band to filter by (this can be passed multiple times)", choices=[
+    ap.add_argument("-b", "--band", action="append", help="Band to filter by (this can be passed multiple times)", choices=[
         "630m",
         "160m",
         "80m",
@@ -35,11 +35,36 @@ def main() -> int:
     ap.add_argument("-m", "--mode", action="append", help="Mode to filter by (this can be passed multiple times)", choices=[
         "cw", "rtty", "psk31", "psk63", "ft8", "ft4"
     ])
+    ap.add_argument("-f", "--filter-call", action="append",
+                    help="Callign to filter by (this can be passed multiple times)")
     args = ap.parse_args()
 
     # Build callback method
     def callback(spotter: str, spotted: str, frequency: float, mode: str, strength: int, time: str):
-        print(f"{spotter} -> {spotted}")
+
+        # Filter out any unwanted modes
+        if args.mode:
+            if mode.lower() not in args.mode:
+                return
+
+        # Filter out any unwanted callsigns
+        if args.filter_call:
+            for call in args.filter_call:
+                call: str = call.upper()
+
+                if call == spotter.upper() or call == spotted.upper():
+                    break
+            else:
+                return
+
+        # Filter out any unwanted bands
+        if args.band:
+            if frequency < band_mapping[args.band][0] or frequency > band_mapping[args.band][1]:
+                return
+
+        # Print the spot
+        print(
+            f"{spotter} spotted {spotted} on {frequency} using {mode} ({strength} dB) at {time}")
 
     # Begin loop
     listen_forever(args.callsign, callback)
